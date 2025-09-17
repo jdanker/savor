8e2c6909894b6f980147c79a8e364dd07cc0640e
Jahred Danker <jahrede@gmail.com>
2025-09-17 18:59:02 -0500

    Initial Commit

---
+++ Apeti/ApetiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApetiApp.swift
+//  Apeti
+//
+//  Created by Jahred Danker on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ApetiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApetiTests/ApetiTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApetiTests.swift
+//  ApetiTests
+//
+//  Created by Jahred Danker on 9/17/25.
+//
+
+import Testing
+@testable import Apeti
+
+struct ApetiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
