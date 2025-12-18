435cf687623cbc42f7d1abde4e925879e6c11956
Jahred Danker <jahrede@gmail.com>
2025-12-18 07:20:39 -0600

    app working with local add

---
+++ Apeti/Views/RootTabView.swift
@@ -0,0 +1,18 @@
+//
+//  RootTabView.swift
+//  Apeti
+//
+//  Created by Jahred Danker on 9/29/25.
+//
+
+import SwiftUI
+
+struct RootTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootTabView()
+}
